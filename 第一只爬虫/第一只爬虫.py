b54a9db283587f37dd5b3f94efcfb4c567bd8541
Willian-wang <a007a007a@sina.cn>
2018-07-27 22:07:58 +0800

    完成多线程爬虫

--- 第一只爬虫/第一只爬虫.py
+++ 第一只爬虫/第一只爬虫.py
@@ -13,50 +13,101 @@ import queue
 
 class CatchDataThread(threading.Thread):
     #顾名思义，这玩意是用来抓取
-    def __init__(self,threadID,Urlqueue,Dataqueue):
+    def __init__(self,threadID,Urlqueue,Dataqueue,lock):
+        threading.Thread.__init__(self)
         self.Urlqueue=Urlqueue
         self.Dataqueue=Dataqueue
         self.threadID=threadID
+        self.lock=lock
         self.headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     def run(self):
+        print(str(threadID)+"号线程开始")
+        global Counter
         while 1:
             if self.Urlqueue.empty():
                 break
+            Counter=Counter+1
+            print(str(Counter)+"/13176")
             num=self.Urlqueue.get()
             url="http://ce.sysu.edu.cn/hope/Diaries/Index_"+str(num)+".aspx"
-            self.Dataqueue.put(RequireData(url))
+            self.Dataqueue.put(RequireData(url,self.headers))
+        print(str(threadID)+"号线程结束")
 
 class DealDataThread(threading.Thread):
-    def __init__(self,threadID,Dataqueue,lock):
-        self.Dataqueue=
+    def __init__(self,threadID,Dataqueue,lock,file):
+        threading.Thread.__init__(self)
+        self.Dataqueue=Dataqueue
+        self.Datalock=lock
+        self.file=file
+        self.athreadID=threadID
+    def run(self):
+        global thread_EXIT
+        print(str(threadID)+"号线程开始")
+        while not thread_EXIT:
+            lock.acquire()
+            if not Dataqueue.empty: 
+                ProduceData(Dataqueue.get())
+                lock.release
+            else:
+                lock.release
+        print(str(threadID)+"号线程结束")
+                
 
-def RequireData(url):
+def RequireData(url,headers):
     data=bytes(urllib.parse.urlencode({'用户名':'谢志强'},{'密码':''}),encoding='utf8')
-    req=urllib.request.Request(url,data=data,headers=headers,method='GET')
+    req=urllib.request.Request(url,headers=headers,method='GET')
     Response=urllib.request.urlopen(req)    
     TEXT=Response.read().decode('utf-8')
     return TEXT
 
+def ProduceData(TEXT):
+    soup=BeautifulSoup(TEXT,"lxml")
+    #print(soup)
+    content=soup.find_all("li",class_="internal_box_left_dairylist_li")
+    #print(content)
+    n=str(n)
+    for item in content:
+        for string_item in item.stripped_strings:
+            #print(string_item)
+            file.write(string_item)
+            file.write('\n')
 
-CATCH_EXIT=False
-PARSE_EXIT = False
 
+thread_EXIT=0
+Counter=0
 
+file=open('日志.txt', 'w',encoding="utf-8") 
 Urlqueue=queue.Queue(13176)
 Dataqueue=queue.Queue()
 threads=[]
+lock=threading.Lock()
+
 for i in range(13176):
     Urlqueue.put(i)
-
-for threadID in range(10):
-    Cthread=CatchDataThread(threadID,Urlqueue,Dataqueue)
+for threadID in range(1,33):
+    thread=CatchDataThread(threadID,Urlqueue,Dataqueue,lock)
     threads.append(thread)
     thread.start()
 
-for threadID in range(11:13):
-    thread=DealDataThread(threadID,Dataqueue,lock)
+while not Urlqueue.empty():
+    pass
 
+for threadID in range(21,22):
+    thread=DealDataThread(threadID,Dataqueue,lock,file)
+    threads.append(thread)
+    thread.start()
 
+while not Dataqueue.empty():
+    pass
+
+thread_EXIT+=1
+
+for t in threads:
+    t.join()
+
+file.close()
+txt_to_excel.txt_to_excel()
+print("exit")
 
 
 
@@ -83,14 +134,14 @@ def getdata(aue,fileObject,n):
     print("Complete %s/13176"% n)
 
 
-n=0
-aue="http://ce.sysu.edu.cn/hope/Diaries/Index_0.aspx"
-fileObject = open('日志.txt', 'a',encoding="utf-8") 
-while n<13176:
-    str1=str(n)
-    str2=str(n+1)
-    aue=aue.replace(str1,str2)
-    getdata(aue,fileObject,n)
-    n=n+1
-fileObject.close()   
-txt_to_excel.txt_to_excel()
+#n=0
+#aue="http://ce.sysu.edu.cn/hope/Diaries/Index_0.aspx"
+#fileObject = open('日志.txt', 'a',encoding="utf-8") 
+#while n<13176:
+#    str1=str(n)
+#    str2=str(n+1)
+#    aue=aue.replace(str1,str2)
+#    getdata(aue,fileObject,n)
+#    n=n+1
+#fileObject.close()   
+#txt_to_excel.txt_to_excel()
